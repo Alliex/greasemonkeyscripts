f295115d5069d8d8a51cd8d6cc79957edcb53962
Alliex <ooaona@gmail.com>
2017-10-09 13:54:34 -0400

    Initial upload testing js

---
+++ AJBProfileMonkey.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        AJB Profile Monkey
+// @namespace   http://userscripts.org/users/438563
+// @include     https://*.abandonedjerks.com
+// @version     1
+// @grant       none
+// ==/UserScript==
+console.log('This script grants no special privileges, so it runs without security limitations.');
+
+var spans = document.getElementsByTagName('span');
+
+for (i=0; i<spans.length; i++) {
+    if(spans[i].getAttribute('itemprop') == 'name') {
+        nametx = spans[i].innerhtml;
+    }
+    spans[i].innerhtml += nametx;
+}
